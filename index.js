f6142edb5619f45a3c93334ecec2c4d39f600412
GaniBaig <86194402+GaniBaig@users.noreply.github.com>
2022-08-26 06:28:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+<div class="sayduck-3d-viewer-container" style="min-height:450px;min-width:300px;width:100%;height:100%;"
+    data-product-uuid="277b03c0-06f6-013b-bb6b-029c71ffe795" data-viewer-options='{"appearance":{"background":"gray"}}'>
+    </div><script defer type="text/javascript" src="https://viewer.sayduck.com"></script>
